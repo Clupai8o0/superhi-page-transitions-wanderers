14d5e3927b6d1cba3a39c0d35c169b2fd183e24e
Clupai8o0 <clupaio4@gmail.com>
2022-07-30 12:08:38 +0300

    adding multiple transitions

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -19,6 +19,12 @@ barba.init({
 	transitions: [
 		{
 			name: "next",
+			custom({ current, next, trigger }) {
+				return (
+					(trigger.classList && trigger.classList.contains("next")) ||
+					trigger === "forward"
+				);
+			},
 			leave({ current, next, trigger }) {
 				return new Promise((resolve) => {
 					const timeline = gsap.timeline({
@@ -81,6 +87,73 @@ barba.init({
 						.to(navigation, { opacity: 1 }, 0)
 						.to(photos, { opacity: 1, x: 0 }, 0)
 						.to(wiper, { x: "100%" }, 0);
+				});
+			},
+		},
+		{
+			name: "previous",
+			leave({ current, next, trigger }) {
+				return new Promise((resolve) => {
+					const timeline = gsap.timeline({
+						onComplete() {
+							current.container.remove();
+							resolve();
+						},
+					});
+
+					const navigation = current.container.querySelectorAll(
+						"header, a.next, a.previous"
+					);
+					const photos = current.container.querySelectorAll("div.photos");
+
+					timeline
+						.set(wiper, { x: "100%" })
+						.set(wiperImg, { opacity: 0 })
+						.set(wiperText, { y: "100%" })
+						.to(navigation, { opacity: 0 }, 0)
+						.to(photos, { opacity: 0.25, x: -500 }, 0)
+						.to(wiper, { x: 0 }, 0);
+				});
+			},
+			beforeEnter({ current, next, trigger }) {
+				wiperText.textContent = next.container.getAttribute("data-title");
+
+				return new Promise((resolve) => {
+					const timeline = gsap.timeline({
+						defaults: {
+							duration: 1,
+						},
+						onComplete() {
+							resolve();
+						},
+					});
+
+					timeline
+						.to(wiperImg, { opacity: 1 }, 0)
+						.to(wiperText, { y: 0 }, 0)
+						.to(wiperText, { y: "100%", delay: 1.5 }, 2)
+						.to(wiperImg, { opacity: 0, delay: 1.5 }, 2);
+				});
+			},
+			enter({ current, next, trigger }) {
+				return new Promise((resolve) => {
+					const timeline = gsap.timeline({
+						onComplete() {
+							resolve();
+						},
+					});
+
+					const navigation = next.container.querySelectorAll(
+						"header, a.next, a.previous"
+					);
+					const photos = current.container.querySelectorAll("div.photos");
+
+					timeline
+						.set(navigation, { opacity: 0 })
+						.set(photos, { opacity: 0.25, x: 500 })
+						.to(navigation, { opacity: 1 }, 0)
+						.to(photos, { opacity: 1, x: 0 }, 0)
+						.to(wiper, { x: "-100%" }, 0);
 				});
 			},
 		},
