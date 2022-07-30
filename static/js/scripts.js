ae4732e4b4341e04355c15d6a1daac1ee73a0a92
Clupai8o0 <clupaio4@gmail.com>
2022-07-30 11:36:21 +0300

    basic fading in for navigation

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,12 +5,32 @@ barba.init({
 			name: "next",
 			leave({ current, next, trigger }) {
 				return new Promise((resolve) => {
-					resolve();
+					const timeline = gsap.timeline({
+						onComplete() {
+							resolve();
+						},
+					});
+
+					const navigation = current.container.querySelectorAll(
+						"header, a.next, a.previous"
+					);
+					timeline.to(navigation, { opacity: 0 });
 				});
 			},
 			next({ current, next, trigger }) {
 				return new Promise((resolve) => {
-					resolve();
+					const timeline = gsap.timeline({
+						onComplete() {
+							resolve();
+						},
+					});
+
+					const navigation = next.container.querySelectorAll(
+						"header, a.next, a.previous"
+					);
+					timeline
+						.set(navigation, { opacity: 0 })
+						.to(navigation, { opacity: 1 });
 				});
 			},
 		},
