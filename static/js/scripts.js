9482dfdf43646b86a3871e2cbe23f46078909ea4
Clupai8o0 <clupaio4@gmail.com>
2022-07-30 11:51:57 +0300

    adding image to wiper

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,11 @@
 const bodyTag = document.querySelector("body");
 const wiper = document.createElement("div");
 wiper.classList.add("wiper");
+
+const wiperImg = document.createElement("img");
+wiperImg.setAttribute("src", "static/assets/logo.svg");
+
+wiper.appendChild(wiperImg);
 bodyTag.appendChild(wiper);
 
 barba.init({
@@ -24,9 +29,23 @@ barba.init({
 
 					timeline
 						.set(wiper, { x: "-100%" })
+						.set(wiperImg, { opacity: 0 })
 						.to(navigation, { opacity: 0 }, 0)
 						.to(photos, { opacity: 0.25, x: 500 }, 0)
 						.to(wiper, { x: 0 }, 0);
+				});
+			},
+			beforeEnter({ current, next, trigger }) {
+				return new Promise((resolve) => {
+					const timeline = gsap.timeline({
+						onComplete() {
+							resolve();
+						},
+					});
+
+					timeline
+						.to(wiperImg, { opacity: 1 })
+						.to(wiperImg, { opacity: 0, delay: 2 });
 				});
 			},
 			enter({ current, next, trigger }) {
